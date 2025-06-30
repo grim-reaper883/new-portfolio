edad49b7795b2318d24d9cceb375de3a884152e9
grim-reaper883 <m.asakib05@gmail.com>
2025-06-30 15:58:58 +0600

    added hero and about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import About from "./components/About";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header/>
+      <main>
+        <Hero/>
+        <About/>
+      </main>
     </>
   );
 }
