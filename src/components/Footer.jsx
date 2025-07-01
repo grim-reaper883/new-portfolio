0b8fe3ed6a2c344599d8670ec204ae6c7eb16b90
grim-reaper883 <m.asakib05@gmail.com>
2025-07-01 21:51:05 +0600

    bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,23 +28,23 @@ const sitemap = [
 const socials = [
   {
     label: 'GitHub',
-    href: 'https://www.github.com/codewithsadee-org'
+    href: ''
   },
   {
     label: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/codewithsadee'
+    href: ''
   },
   {
     label: 'Twitter X',
-    href: 'https://x.com/codewithsadee_'
+    href: ''
   },
   {
     label: 'Instagram',
-    href: 'https://www.instagram.com/codewithsadee'
+    href: ''
   },
   {
     label: 'CodePen',
-    href: 'https://codepen.io/codewithsadee'
+    href: ''
   }
 ];
 
